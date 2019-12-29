f9b6e216888b4f6ab940cac5b056049c133eb770
lhzjoker <2313169248@qq.com>
2019-12-29 18:19:46 +0800

    springboot连接jdbc

---
+++ spring-boot-06-data-jdbc/src/main/java/com/atguigu/springbootdata/SpringBootData06JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springbootdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootData06JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootData06JdbcApplication.class, args);
+	}
+
+}
---
+++ spring-boot-06-data-jdbc/src/test/java/com/atguigu/springbootdata/SpringBootData06JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.springbootdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootData06JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
